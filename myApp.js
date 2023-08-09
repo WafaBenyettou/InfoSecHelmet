19d3268a0b427f969d4e050971cf0128b45e2de6
WafaBenyettou <benwafa951@gmail.com>
2023-08-09 20:43:43 +0100

    Mitigate the Risk of Clickjacking with helmet

--- myApp.js
+++ myApp.js
@@ -1,20 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
-
-let helmet = require('helmet');
+let helmet = require("helmet");
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-
+app.use(helmet.frameguard({ action: "deny" }));
 
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+const api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
