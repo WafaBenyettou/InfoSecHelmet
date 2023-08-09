8165d1d9442b8144b17b37aea638f5dc56f7d31c
WafaBenyettou <benwafa951@gmail.com>
2023-08-09 21:06:54 +0100

    Fixing err

--- myApp.js
+++ myApp.js
@@ -9,7 +9,8 @@ app.use(helmet.frameguard({ action: "deny" }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({ maxAge: 7776000, force: true }));
+const ninetyDaysInSeconds = 90*24*60*60
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
 app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] } }));
